8231695bbe51bf63ed5313426ed6a3387bb5c3bc
Richard Williams <richard.williams@manchester.ac.uk>
2020-04-30 10:57:24 +0100

    Add banner if new service worker found

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-document.body.style.background = 'blue';
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      console.log('SW: controllerchange');
+      // publish('NEW_SW_CONTROLLING');
+    });
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, (err) => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// Do offline stuff
+if (navigator.onLine === false) { 
+  document.body.classList.add('offline'); 
+}
